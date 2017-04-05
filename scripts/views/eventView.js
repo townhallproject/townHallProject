fb63f6a2a6d17819fbf9cb8c9a4fceeabe7fe2b7
Megan Riel-Mehan <meganrm@gmail.com>
2017-04-04 22:59:59 -0700

    issue 103 text change

--- scripts/views/eventView.js
+++ scripts/views/eventView.js
@@ -130,21 +130,21 @@
     var button = '<li><button class="btn btn-secondary btn-xs" ' +
                  'data-filter="' + filter + '" data-value="' + value + '" >' +
                     value + '<i class="fa fa-times" aria-hidden="true"></i>' +
-                  '</button></li>'
+                  '</button></li>';
     $('#filter-info').append(button);
-  }
+  };
 
   eventHandler.removeFilter = function() {
     var $button = $(this);
     TownHall.removeFilter($button.attr('data-filter'), $button.attr('data-value'));
     eventHandler.renderTableWithArray(eventHandler.getFilterState());
     $button.parent().remove();
-  }
+  };
 
   eventHandler.resetFilters = function() {
     TownHall.resetFilters();
     $('#filter-info li button').parent().remove();
-  }
+  };
   // filters the table on click
   eventHandler.filterTable = function (e) {
     e.preventDefault();
@@ -246,7 +246,7 @@
         eventHandler.addFilter(this.$element.attr('data-filter'), selection);
         eventHandler.renderTableWithArray(eventHandler.getFilterState());
       }
-    }
+    };
 
     $("#stateTypeahead").typeahead($.extend({source: TownHall.allStates}, typeaheadConfig));
     $("#memberTypeahead").typeahead($.extend({source: TownHall.allMoCs}, typeaheadConfig));
