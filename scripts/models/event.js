3e77ad2901c2dd9c10932c939aca4955aedb3ab9
Megan Riel-Mehan <meganrm@gmail.com>
2017-04-15 14:03:58 -0700

    Removed DC events and Coffee events from zip lookup

--- scripts/models/event.js
+++ scripts/models/event.js
@@ -124,7 +124,7 @@
     var locations = [];
     return firebase.database().ref('/townHalls').once('value').then(function(snapshot) {
       snapshot.forEach(function(ele){
-        if (ele.val().StateAb !== 'DC') {
+        if (ele.val().meetingType !== 'DC Event' && ele.val().meetingType !== 'Coffee') {
           locations.push(new TownHall(ele.val()));
         }
       });
