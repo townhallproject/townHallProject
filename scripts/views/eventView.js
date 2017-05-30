8837765a75c1df38b56a60dd05b2446d5d8728d0
meganr <meganrm@gmail.com>
2017-05-29 23:17:21 -0700

    reduced copied code

--- scripts/views/eventView.js
+++ scripts/views/eventView.js
@@ -31,10 +31,10 @@
     });
 
     var justSenate = true;
-    var numOfDistrictEvents = 0
+    var numOfDistrictEvents = 0;
     selectedData.forEach(function(ele){
       if(ele.District !== 'Senate') {
-        numOfDistrictEvents ++
+        numOfDistrictEvents ++;
         justSenate = false;
       }
     });
@@ -53,7 +53,7 @@
       eventHandler.renderTableWithArray(selectedData);
       mapView.makeSidebar(selectedData);
       var numOfSateEvents = selectedData.length - numOfDistrictEvents;
-      var message = '<p>Showing ' + numOfDistrictEvents + ' event(s) for ' + districtText + ' representatives</p>';
+      var message = '<p>Showing ' + numOfDistrictEvents + ' event(s) for the ' + districtText + ' representative</p>';
       var messageState = '<p>and ' + numOfSateEvents + ' event(s) for ' + thisState + ' senators</p>';
       $text.html(message + messageState);
 
@@ -145,29 +145,35 @@
     $panel.appendTo($parent);
   };
 
+  // renders the results of rep response
+  eventHandler.repCards = function(results, compiledTemplate, $parent) {
+    results.forEach(function(rep) {
+      switch(rep.party) {
+      case 'R':
+        rep.party = 'Republican';
+        break;
+      case 'D':
+        rep.party = 'Democrat';
+        break;
+      case 'I':
+        rep.party = 'Independent';
+        break;
+      }
+      var termEnd = new Date(rep.term_end);
+      // If term expires in janurary then assume the election is in the prior year
+      rep.electionYear = termEnd.getMonth() === 0 ? termEnd.getFullYear() - 1 : termEnd.getFullYear();
+      $parent.append(compiledTemplate(rep));
+    });
+  };
+
   // Display a list of reps with contact info
   eventHandler.renderRepresentativeCards = function(representativePromise, $parent, state) {
     $parent.empty(); // If they search for a new zipcode clear the old info
     representativePromise.success(function(representatives) {
       var compiledTemplate = Handlebars.getTemplate('representativeCard');
       $parent.append('<h2 class="text-primary text-center">Your Representatives</h2>');
-      representatives.results.forEach(function(rep) {
-        switch(rep.party) {
-        case 'R':
-          rep.party = 'Republican';
-          break;
-        case 'D':
-          rep.party = 'Democrat';
-          break;
-        case 'I':
-          rep.party = 'Independent';
-          break;
-        }
-        var termEnd = new Date(rep.term_end);
-        // If term expires in janurary then assume the election is in the prior year
-        rep.electionYear = termEnd.getMonth() === 0 ? termEnd.getFullYear() - 1 : termEnd.getFullYear();
-        $parent.append(compiledTemplate(rep));
-      });
+      eventHandler.repCards(representatives.results, compiledTemplate, $parent);
+
       if (representatives.results.length > 3) {
         $parent.append('<h4 class="col-md-12 text-center">Your zip code encompasses more than one district.<br><small><a href="http://www.house.gov/representatives/find/">Learn More</a></small></h4>');
       } else if (representatives.results.length === 1) {
@@ -176,30 +182,11 @@
     });
   };
 
-  // append additional reps
+  // append additional reps for lookup by district
   eventHandler.addRepresentativeCards = function(representativePromise, $parent) {
     representativePromise.success(function(representatives) {
       var compiledTemplate = Handlebars.getTemplate('representativeCard');
-      representatives.results.forEach(function(rep) {
-        switch(rep.party) {
-        case 'R':
-          rep.party = 'Republican';
-          break;
-        case 'D':
-          rep.party = 'Democrat';
-          break;
-        case 'I':
-          rep.party = 'Independent';
-          break;
-        }
-        var termEnd = new Date(rep.term_end);
-        // If term expires in janurary then assume the election is in the prior year
-        rep.electionYear = termEnd.getMonth() === 0 ? termEnd.getFullYear() - 1 : termEnd.getFullYear();
-        $parent.append(compiledTemplate(rep));
-      });
-      if (representatives.results.length > 3) {
-        $parent.append('<h4 class="col-md-12 text-center">Your zip code encompasses more than one district.<br><small><a href="http://www.house.gov/representatives/find/">Learn More</a></small></h4>');
-      }
+      eventHandler.repCards(representatives.results, compiledTemplate, $parent);
     });
   };
 
