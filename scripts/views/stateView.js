2e4da0e097220b2397177c3b507312eecec240a9
Raegan Millhollin <desdemonhu@gmail.com>
2017-12-02 14:55:22 -0800

    Now accounts for states with dashes in the url

--- scripts/views/stateView.js
+++ scripts/views/stateView.js
@@ -7,12 +7,19 @@
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+///Turns state variable with dashes into the state name
+  function parseState(string){
+    let nameArray = string.split('-').map(function(name){
+      return capitalizeFirstLetter(name);
+    });
+    return nameArray.join(' ');
+  }
+
   stateView.getState = function(ctx, next){
     ///convert state name to necessary coordinates for mapbox url
     let state = $.grep(stateData, function(e){
-      return e.Name === capitalizeFirstLetter(ctx.params.state);
+      return e.Name === parseState(ctx.params.state);
     });
-
     if(state.length > 0){
       let stateUsps = state[0]['USPS'];
       stateView.stateCoords = bboxes[stateUsps];
