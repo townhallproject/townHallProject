058f186aa07a705b608b7a9b4d58e9b46f9fa15f
meganr <meganrm@gmail.com>
2017-12-26 17:37:39 -0800

    just using redirects with firebase, and pages for routing

--- functions/index.js
+++ functions/index.js
@@ -11,5 +11,13 @@ exports.stateRouting = functions.https.onRequest((req, res) => {
   req.url = url.parse(req.url);
   req.state = req.url.pathname.split('/')[1]
   console.log('state', req.state);
-  res.sendFile('index.html', {root: '../'});
+  // res.sendFile('index.html', {root: 'https://townhallproject-86312.firebaseapp.com/'});
+  res.status(200).send(`<!doctype html>
+  <head>
+    <title>Time</title>
+  </head>
+  <body>
+    ${__dirname}
+  </body>
+</html>`);
 });
