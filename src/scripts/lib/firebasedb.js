b08ff8d29436795e1dc044adf89b6d347e695c1b
Alan Magelssen <alanmagelssen@gmail.com>
2018-11-07 21:49:16 -0800

    wip webpack build

--- src/scripts/lib/firebasedb.js
+++ src/scripts/lib/firebasedb.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAmCMim_2ePjXmiPrFhscbuBOr0updtryc',
-  authDomain: 'thp-dev-db.firebaseapp.com',
-  databaseURL: 'https://thp-dev-db.firebaseio.com',
-  projectId: 'thp-dev-db',
-  storageBucket: 'thp-dev-db.appspot.com',
-  messagingSenderId: '426223839812'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 firebase.initializeApp(config);
