fb4487e20b949141ac7d5e7109137252a4dcb680
Alan Magelssen <alanmagelssen@gmail.com>
2019-01-30 22:01:21 -0800

    wip banner year end

--- scripts/views/eventView.js
+++ scripts/views/eventView.js
@@ -466,9 +466,9 @@
 
   function setYearEndImage(){
     if (window.innerWidth < 768) {
-      $('#year-one img').attr('src', 'Images/EOY_2017_Report_Mobile.png');
+      $('#year-two img').attr('src', 'Images/lookback2018-Mobile.png');
     } else {
-      $('#year-one img').attr('src', 'Images/EOY_Report_Layout_noBG-01-01.png');
+      $('#year-two img').attr('src', 'Images/lookback2018-Desktop-nobg.png');
 
     }
   }
