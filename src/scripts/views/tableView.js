d29264d1e2da73a83d7a5d95bf8e95663a2b5fb1
Alan Magelssen <alanmagelssen@gmail.com>
2019-08-22 13:54:12 -0700

    Add Voting Rights and Youth Vote to default and dropdown filter

--- src/scripts/views/tableView.js
+++ src/scripts/views/tableView.js
@@ -30,6 +30,8 @@ tableHandler.initialFilters = function () {
   tableHandler.addFilter('meetingType', 'Town Hall');
   tableHandler.addFilter('meetingType', 'Empty Chair Town Hall');
   tableHandler.addFilter('meetingType', 'Campaign Town Hall');
+  tableHandler.addFilter('meetingType', 'Youth Vote');
+  tableHandler.addFilter('meetingType', 'Voting Rights');
 };
 
 tableHandler.configureDropdowns = function() {
