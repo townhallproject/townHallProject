1af7e37190628c833eaaf0ecaa75a3b108d95c1a
Alan Magelssen <alanmagelssen@gmail.com>
2019-10-20 23:08:09 -0700

    add yearly report component

--- src/components/EndOfYearReport/index.js
+++ src/components/EndOfYearReport/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const EndOfYearReport = (props) => {
   const {
-    hashtag,
     imageSrc,
   } = props;
   return (
-    <div role="tabpanel" className="tab-pane" id={hashtag}>
-      <div className="container">
-        <img src={imageSrc} alt="" />
-      </div>
+    <div className="container">
+      <img src={imageSrc} alt="" />
     </div>
   )
 };
