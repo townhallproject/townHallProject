b2bead7f15d3bf380a57b9513da54ee4af679492
Alan Magelssen <alanmagelssen@gmail.com>
2019-10-20 23:06:14 -0700

    add submit component

--- src/components/SubmitEventForm/index.js
+++ src/components/SubmitEventForm/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const SubmitEventForm = (props) => {
   return (
-    <div role="tabpanel" className="tab-pane" id="submit">
+    <React.Fragment>
       <div className='embed-container submit-embed-container'>
         <iframe
           src='https://docs.google.com/forms/d/e/1FAIpQLSfvIJikraQCZcqUpYfDZHC7KvxDUp4zcfzlLQ7RoaDQcBZUbQ/viewform?c=0&w=1'
-          style={{ borderWidth: 0, width: 800, height: 600, frameborder: 0 }}></iframe>
+          style={{ borderWidth: 0, frameborder: 0 }}></iframe>
       </div>
-    </div>
+    </React.Fragment>
   )
 };
 
