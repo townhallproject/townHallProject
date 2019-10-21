1588107a387079b21b11b0a9ee44ca1766557f08
Alan Magelssen <alanmagelssen@gmail.com>
2019-10-20 23:08:17 -0700

    add contact page component

--- src/components/ContactUsForm/index.js
+++ src/components/ContactUsForm/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ContactUsForm = (props) => {
   return (
-    <div role="tabpanel" className="tab-pane" id="contact">
+    <React.Fragment>
       <div className='embed-container contact-embed-container'>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSffVaop5xVSxXFrRtAZsxMDqkvucAplMdC9-CPHiTXxPKQV0g/viewform?embedded=true"
           width="800" height="600" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
       </div>
-    </div>
+    </React.Fragment>
   )
 };
 
