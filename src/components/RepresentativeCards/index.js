7251f296e6561bbccc00d91b11309ca8445edae3
Alan Magelssen <alanmagelssen@gmail.com>
2019-11-24 12:06:38 -0800

    rep card change wip

--- src/components/RepresentativeCards/index.js
+++ src/components/RepresentativeCards/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MoC from '../models/MemberOfCongress';
+import RepCard from '../../templates/representativeCard.jsx'
 
 // import './style.scss';
 
-const RepresentativeCards = (props) => {
-  return (
-    <div id="representativeCards">
-      <section className="container container-fluid"></section>
-    </div>
-  );
+class RepresentativeCards extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {
+      reps
+    } = this.props;
+    return (
+      <div id="representativeCards">
+        <section className="container container-fluid">
+          {
+            reps.map((rep) => {
+              const newRep = new MoC(rep);
+              newRep.format();
+              return (
+                <RepCard rep={newRep} />
+              );
+            })
+          }
+        </section>
+      </div>
+    );
+  }
 }
 
 export default RepresentativeCards;
