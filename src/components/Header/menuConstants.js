83b9f38d239394e93a76e5f9542f804c31fe7ea3
meganrm <meganrm@gmail.com>
2019-12-17 23:48:34 -0800

    adding display and link name to menu items

--- src/components/Header/menuConstants.js
+++ src/components/Header/menuConstants.js
@@ -1,8 +1,51 @@
+export const SIGN_UP_LINK = 'join';
+export const VOLUNTEER_LINK = 'volunteer';
+export const ORGANIZE_A_TOWN_HALL_LINK = 'organize';
+export const SHARE_LINK = 'share';
+export const MISSING_MEMBER_LINK = 'missing-members';
+export const TOWN_HALL_PLEDGE_LINK = 'https://www.townhallpledge.com/';
+export const FIGHT_FOR_OUR_DEMO_LINK = 'http://fightforourdemocracy.com/';
+export const ABOUT_LINK = 'about';
+export const NEWS_LINK = 'news';
+export const RESEARCH_LINK = 'research';
+
 export const MENU_MAP = new Map([
   ['submit-event', []],
-  ['take-action', ['Get Town Hall Alerts', 'Volunteer with THP', 'Organize a Town Hall', 'Share your Town Hall Experience']],
-  ['our-projects', ['Missing Members', 'Town Hall Pledge', 'Fight for Our Democracy', 'State Legislatures']],
-  ['learn-more', ['About THP', 'In the News', 'Our Research']],
+  ['take-action', [{
+    display: 'Get Town Hall Alerts',
+    link: SIGN_UP_LINK
+  }, {
+    display: 'Volunteer with THP',
+    link: VOLUNTEER_LINK
+  }, {
+    display: 'Organize a Town Hall',
+    link: ORGANIZE_A_TOWN_HALL_LINK
+  }, {
+    display: 'Share your Town Hall Experience',
+    link: SHARE_LINK
+  }]],
+  ['our-projects', [{
+    display: 'Missing Members',
+    link: MISSING_MEMBER_LINK
+  }, {
+    display: 'Town Hall Pledge',
+    link: TOWN_HALL_PLEDGE_LINK
+  }, {
+    display: 'Fight for Our Democracy',
+    link: FIGHT_FOR_OUR_DEMO_LINK
+  }, {
+    display: 'State Legislatures'
+  }]],
+  ['learn-more', [{
+    display: 'About THP',
+    link: ABOUT_LINK
+  }, {
+    display: 'In the News',
+    link: NEWS_LINK
+  }, {
+    display: 'Our Research',
+    link: RESEARCH_LINK
+  }]],
   ['donate', []],
 ]);
 
