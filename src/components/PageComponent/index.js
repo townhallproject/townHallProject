069e44be011735d4dc74c0f861bcd63eb335a1f4
meganrm <meganrm@gmail.com>
2020-01-03 12:35:20 -0800

    removed banner

--- src/components/PageComponent/index.js
+++ src/components/PageComponent/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Banner from '../Banner';
 
 const PageComponent = (props) => {
   const {
@@ -11,11 +10,6 @@ const PageComponent = (props) => {
   const activeClass = active ? "active" : "";
   return (
     <div role="tabpanel" className={["tab-pane", "page", activeClass].join(" ")} id={id}>
-    {
-      activeBanner && (
-        <Banner />
-      )
-    }
       {children}
     </div>
   );
