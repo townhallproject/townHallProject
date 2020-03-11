479e93f26c3a259e3f992c88ad23ef80b9b72060
Krista Loven <krista.loven.87@gmail.com>
2020-03-11 09:27:03 -0700

    Public health warning banner for events

---
+++ src/components/Home/EventsWarningBanner/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.less'
+
+const EventsWarningBanner = () => {
+  return (
+    <div className="warningBanner">
+      <h3>
+        Face-to-face conversations between Americans and their lawmakers are vital for our democracy.
+      </h3>
+      <p>
+        At the same time, we encourage everyone to follow the most up-to-date CDC public health guidelines. We are updating events as rapidly as possible as we learn of postponements, rescheduling, or shifts to virtual meetings.
+      </p>
+      <p>
+        We strongly encourage all lawmakers to continue to find ways to stay in contact with their constituents.
+      </p>
+    </div>
+  )
+}
+
+export default EventsWarningBanner
