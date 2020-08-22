cb27db029f760f71932c76ea6f3443d82d96d6f1
meganrm <meganrm@gmail.com>
2020-08-21 21:39:08 -0700

    wip- antd mobile

---
+++ src/components/Header/MobileMenu.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Menu, ActivityIndicator, NavBar } from "antd-mobile";
+import 'antd-mobile/lib/menu/style' //TODO figure out why babel-imports isn't importing style
+import {
+  MENU_MAP,
+  STATE_LEGISLATURES_MENU,
+  MISSING_MEMBER_LINK,
+} from "./menuConstants";
+
+const data = []
+MENU_MAP.forEach((subMenu, key) => {
+  data.push({
+    value: key,
+    label: key,
+    children: subMenu
+  })
+})
+class MobileMenu extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      initData: '',
+      show: false,
+    };
+  }
+  onChange = (value) => {
+    let label = '';
+    data.forEach((dataItem) => {
+      if (dataItem.value === value[0]) {
+        label = dataItem.label;
+        if (dataItem.children && value[1]) {
+          dataItem.children.forEach((cItem) => {
+            if (cItem.value === value[1]) {
+              label += ` ${cItem.label}`;
+            }
+          });
+        }
+      }
+    });
+    console.log(label);
+  }
+  handleClick = (e) => {
+    e.preventDefault(); // Fix event propagation on Android
+    this.setState({
+      show: !this.state.show,
+    });
+    // mock for async data loading
+    if (!this.state.initData) {
+      setTimeout(() => {
+        this.setState({
+          initData: data,
+        });
+      }, 500);
+    }
+  }
+
+  onMaskClick = () => {
+    this.setState({
+      show: false,
+    });
+  }
+
+  render() {
+    const { initData, show } = this.state;
+    const menuEl = (
+      <Menu
+        className="foo-menu"
+        data={initData}
+        value={['1', '3']}
+        onChange={this.onChange}
+        height={document.documentElement.clientHeight * 0.6}
+      />
+    );
+    const loadingEl = (
+      <div style={{ width: '100%', height: document.documentElement.clientHeight * 0.6, display: 'flex', justifyContent: 'center' }}>
+        <ActivityIndicator size="large" />
+      </div>
+    );
+    return (
+      <div className={show ? 'menu-active' : ''}>
+        <div>
+          <NavBar
+            leftContent="Menu"
+            mode="light"
+            icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/iXVHARNNlmdCGnwWxQPH.svg" className="am-icon am-icon-md" alt="" />}
+            onLeftClick={this.handleClick}
+            className="top-nav-bar"
+          >
+            Here is title
+          </NavBar>
+        </div>
+        {show ? initData ? menuEl : loadingEl : null}
+        {show ? <div className="menu-mask" onClick={this.onMaskClick} /> : null}
+      </div>
+    );
+  }
+}
+
+export default MobileMenu;
